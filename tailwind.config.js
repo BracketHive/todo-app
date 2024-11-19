bb6bf02052f7e24b5413ff37e53272044b0c2b0e
BracketHive <info@brackethive.com>
2024-11-19 16:46:51 +0200

    chore: installed required dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "{srcDir}/components/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "{srcDir}/pages/**/*.{vue,js,jsx,mjs,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
