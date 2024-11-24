ead6024e1a1790a5d0b29c091ceccd80888ced99
BracketHive <info@brackethive.com>
2024-11-24 12:31:41 +0200

    feat: link on todo app title for home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+const router = useRouter()
+</script>
+
 <template>
   <div class="font-body !text-[17px] w-screen h-screen bg-[#F1F1F2]">
     <div>
       <div class="bg-white mx-auto py-10 text-center">
-        <h1 class="text-2xl font-bold">TO DO LIST</h1>
+        <h1 class="text-2xl font-bold cursor-pointer" @click="router.push('/')">TO DO LIST</h1>
       </div>
 
       <slot />
